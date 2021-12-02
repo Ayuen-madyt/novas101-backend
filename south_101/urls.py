7ef7cd7f95a8b9d3226262488eb28076a18b58cb
32dantey <nndurere32@gmail.com>
2021-12-02 17:31:47 +0300

    deleted pipfile

--- south_101/urls.py
+++ south_101/urls.py
@@ -17,11 +17,11 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
 # where i include templates from  react build folder
-# urlpatterns += [
-#     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+urlpatterns += [
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
     
-#     password reset confirm url
-#      re_path(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-#         PasswordResetConfirmView.as_view(),
-#         name='password_reset_confirm'),
-#     ]
+    # password reset confirm url
+    #  re_path(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     PasswordResetConfirmView.as_view(),
+    #     name='password_reset_confirm'),
+    ]
